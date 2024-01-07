3346ee252c4c856831593a77777ea88e1280ae50
Vaibhav Isloor <viisloor@gmail.com>
2024-01-07 21:44:07 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,16 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.minsize(width=500,height=500)
+window = Tk()
+window.title("GUI Program")
+window.minsize(width=500, height=500)
 
-window.mainloop()
+
+label=Label(text="Sample label", font=('Arial',24,'bold'))
+label.pack() #.pack() places it on the screen
+label["text"]="Hello"
+
+button=Button(text="Click me")
+# button.pack(side="left")
+button.pack()
+
+window.mainloop() #This ensures that the window is listening for user input
