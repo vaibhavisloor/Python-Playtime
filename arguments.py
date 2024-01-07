92cfa0d9d9493c3f9365d380ee2e4f11e589e46a
Vaibhav Isloor <viisloor@gmail.com>
2024-01-07 21:43:05 +0530

    args file added

---
+++ arguments.py
@@ -0,0 +1,27 @@
+def add(*args): 
+    sum=0
+    for n in args:
+        sum+=n
+    print(sum)
+
+add(1,2,3,4,5,6,7,8,9,10)        
+
+# -------------------------------
+
+class PC:
+    def __init__(self,**kw):
+        self.make=kw.get("make")
+        self.model=kw.get("model")
+
+one = PC(make="HP")
+print(one.model)        
+print(one.make)      
+
+# ------------------------------------
+
+def display(**kwargs):
+    for x,y in kwargs.items():
+        print(y)
+
+display(n="Vai",a=20, p=305)
+
