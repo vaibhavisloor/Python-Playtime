a8679c25ab64ff91e9addf72c9bdcc4cc654af98
Vaibhav Isloor <viisloor@gmail.com>
2024-01-17 21:56:29 +0530

    timer_added_1

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -22,21 +22,28 @@ window.config(padx=100,pady=50,bg=YELLOW)
 
 label1 = Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,40,"bold"))
 label1.grid(row=0,column=1)
+
 canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="pomodoro/tomato.png")
 canvas.create_image(100,112,image=tomato_img)
 canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
 canvas.grid(row=1,column=1)
 
+def timer(count):
+   if count >= 0: 
+    print(count)
+    window.after(1000,timer,count-1)
+
+
+timer(5)
 
 start_button = Button(text="start",font=(FONT_NAME),highlightthickness=0)
 start_button.grid(row=2,column=0)
 
-reset_button = Button(text="restart",font=(FONT_NAME),highlightthickness=0)
-reset_button.grid(row=2,column=2)
-
-
 check_marks = Label(text="✔",fg=GREEN,bg=YELLOW,font=(FONT_NAME,20))
 check_marks.grid(row=3,column=1)
+
+reset_button = Button(text="restart",font=(FONT_NAME),highlightthickness=0)
+reset_button.grid(row=2,column=2)
 
 window.mainloop()
