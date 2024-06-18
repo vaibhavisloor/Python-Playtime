81bd39cbab18e9419a55b34c39e2fa74b3577fb6
Vaibhav Isloor <viisloor@gmail.com>
2024-06-18 18:31:26 +0530

    Stock App

--- stock-news-normal-start/main.py
+++ stock-news-normal-start/main.py
@@ -4,34 +4,6 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
 import requests
 from twilio.rest import Client
 from dotenv import load_dotenv
@@ -43,11 +15,9 @@ COMPANY_NAME = "Tesla Inc."
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-
-
-STOCK_API_KEY="V3J813NP8GY0MS35"
-
-NEWS_API_KEY="4e7f83279506465ba385808b5cd8b5df"
+load_dotenv()
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+STOCK_API_KEY = os.getenv('STOCK_API_KEY')
 
 stock_params={
     "function":"TIME_SERIES_DAILY",
@@ -67,14 +37,13 @@ day_before_yesterday_closing_price = round(float(prices_list[1]['4. close']))
 print(day_before_yesterday_closing_price)
 
 
-difference = abs(round(yesterday_closing_price - day_before_yesterday_closing_price,2))
+difference = round(yesterday_closing_price - day_before_yesterday_closing_price)
 print(difference)
 
-percentage_difference = (difference / day_before_yesterday_closing_price) * 100
+percentage_difference = (abs(difference) / day_before_yesterday_closing_price) * 100
 
 
-if percentage_difference >= 0: 
-# if True: 
+if percentage_difference >= 5: 
     news_params={
         "apiKey":NEWS_API_KEY,
         'qInTitle':COMPANY_NAME,
@@ -86,30 +55,23 @@ if percentage_difference >= 0:
     print(title)
 
 
-load_dotenv()
-account_sid = os.getenv['account_sid']
-auth_token = os.getenv['auth_tok']
+account_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
 client = Client(account_sid, auth_token)
 
-text = f'This is the latest news regarding {COMPANY_NAME} :\n'
+if difference > 0 :
+    symbol = '🔺'
+else:
+    symbol = '🔻'
+
+
+text = f' Today the stock moved by {round(percentage_difference,2)} {symbol}.\nThis is the latest news regarding {COMPANY_NAME} :\n'
 for t in title:
     text+=f'{t}\n'    
 
+print(text)
 message = client.messages.create(
                               body=text,
                               from_='+19523142296',
                               to='+91 99008 55730'
                           )
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
