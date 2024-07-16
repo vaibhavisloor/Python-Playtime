3691d52b0f493b2aacea46012095ccb065588160
Vaibhav Isloor <viisloor@gmail.com>
2024-07-16 12:22:13 +0530

    Amazon-Price-Tracker

--- Amazon-Price-Tracker/main.py
+++ Amazon-Price-Tracker/main.py
@@ -1,11 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
-URL_ENDPOINT = "https://appbrewery.github.io/instant_pot/"
-response = requests.get(URL_ENDPOINT)
+import smtplib
 
+mail = "viisloor@gmail.com"
+passw = "" #Get this from Google app passwords
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,kn;q=0.7'
+}
+
+URL_ENDPOINT = "https://appbrewery.github.io/instant_pot/"
+response = requests.get(URL_ENDPOINT,headers=headers)
+# print(response.text)
 
 soup = BeautifulSoup(response.content,"html.parser")
 tags = soup.find_all(class_="priceToPay")
 for tag in tags:
     cost=tag.text
-print(cost)    
+if cost < 99.99:
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(user = mail , password=passw)
+    connection.sendmail(from_addr=mail, to_addrs="receiver@gmail.com",msg="Subject:Trying SMTP\n\nSent from smtplib")
+    connection.close()
+else:
+    pass    
