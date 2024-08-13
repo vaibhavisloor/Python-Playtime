d691d38069be2df3349b1374ba15aa5a7c20cb76
Vaibhav Isloor <viisloor@gmail.com>
2024-08-13 14:03:57 +0530

    flask-contd

--- Flask_Tutorial/server.py
+++ Flask_Tutorial/server.py
@@ -1,11 +1,17 @@
 from flask import Flask,render_template
+import requests
+import json
 
 app=Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/guess/<name>")
+def guess_age(name):
+    params={
+        "name" : name
+    }
+    endpoint = "https://api.agify.io/"
+    response = (requests.get(endpoint,params=params)).json()
+    return render_template("index.html",name=response["name"],age=response["age"])
 
 if __name__ == "__main__":
     app.run(debug=True)
