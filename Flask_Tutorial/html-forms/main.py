7ea2fae4f631d84e368ca20ffe482160e78a57d2
Vaibhav Isloor <viisloor@gmail.com>
2024-08-14 12:06:26 +0530

    html-forms-flask

---
+++ Flask_Tutorial/html-forms/main.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/login",methods=['POST','GET'])
+def receive_data():
+    name = request.form["uname"]
+    passw = request.form["pass"]
+    print(name)
+    print(passw)
+    return render_template("value.html",name=name,passw=passw)
+
+if __name__ == "__main__":
+    app.run(debug=True)
