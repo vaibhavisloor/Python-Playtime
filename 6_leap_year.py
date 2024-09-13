a57493095b7c5c8aeb88dcf70dbda1438e39cadd
Vaibhav Isloor <viisloor@gmail.com>
2024-09-13 17:03:37 +0530

    comprev2

--- 6_leap_year.py
+++ 6_leap_year.py
@@ -1,6 +1,6 @@
 year = int(input("Enter the year "))
 
-if(year%4==0 or year%400==0):
+if((year%4==0 and year%100!=0 ) or year%400==0):
     print("It is a leap year");
 else:
     print("It is not a leap year");
