9965399d91bb252880dbe6373fcc7a23a2b6057a
Vaibhav Isloor <viisloor@gmail.com>
2024-09-19 17:19:22 +0530

    comprev6

--- 16_fizzbuzz.py
+++ 16_fizzbuzz.py
@@ -4,6 +4,6 @@ for x in range (1,101):
     elif x%5 == 0 and x%3 != 0:
         print("Buzz")
     elif x%3 == 0 and x%5 == 0: 
-        print("FizzBUzz")   
+        print("FizzBuzz")   
     else:
         print(x)
