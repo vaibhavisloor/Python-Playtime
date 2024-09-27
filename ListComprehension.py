1255f653cb540b836520af7b9cfd6911c20676fe
Vaibhav Isloor <viisloor@gmail.com>
2024-09-27 10:41:43 +0530

    comprev10

--- ListComprehension.py
+++ ListComprehension.py
@@ -24,12 +24,25 @@ names = ["Alex", "Hales", "Beth", "Caroline", "Dave", "Freddie", "Eleanor"]
 
 # new_dict = {new_key : new_value for (key,value) in dict.items() if test}
 
-import random
-students_score = {student:random.randint(1,100) for student in names}
-passed_students = {student:score for (student,score) in students_score.items() if score>=40}
-print(passed_students)
+# import random
+# students_score = {student:random.randint(1,100) for student in names}
+# passed_students = {student:score for (student,score) in students_score.items() if score>=40}
+# print(passed_students)
 
 
+# name= "Vaibhav"
+
+# nested_list = [[1, 2, 3], [4, 5], [6, 7, 8]]
+# # Expected output: [1, 2, 3, 4, 5, 6, 7, 8]
 
 
+# lists = [x for letter in nested_list for x in letter]
+# print(lists)
 
+
+words = ["hello", "world", "python"]
+# Expected output: ['olleh', 'dlrow', 'nohtyp']
+
+num=5
+lists = [num*i for i in range(1,11)]
+print(lists)
