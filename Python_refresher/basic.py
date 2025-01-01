d9e2047dd2b954c4026bd9886c8b99075547196b
Vaibhav Isloor <viisloor@gmail.com>
2025-01-01 14:24:52 +0530

    python-b2b

---
+++ Python_refresher/basic.py
@@ -0,0 +1,31 @@
+# x = input("Enter a letter: ")
+# x=x[0]
+
+# if x in ('a','e','i','o','u'):
+#     print("Vowel")
+# else:
+#     print("Consonant")
+
+
+# print("welocome to the tip calculator")
+
+# bill = float(input("Whats the total bill"))
+# tip=int(input("What percent of the bill would you like to pay as tip"))
+
+# people=int(input("How many people to split the bill"))
+
+# total_amount = bill + (bill*tip/100)
+
+
+# print(f"Each person must pay Rs.{total_amount/people}")
+
+
+# val = 2.1212
+# val1=format(val,".2f")
+# print(val1)\
+
+
+
+words = ["hello","world","my","name","is","Anna"]
+sentence = " & ".join(words)
+print(sentence)
