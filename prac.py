2195ede690b3d2eefc196591bb6a9c2b386657b9
Vaibhav Isloor <viisloor@gmail.com>
2026-03-01 11:32:33 +0530

    bottom-up-memoization

--- prac.py
+++ prac.py
@@ -440,3 +440,17 @@ n = 6
 memo = [-1] * (n+1)
 
 print(fibbonacci(6,memo))
+
+
+def fib(n):
+    if n<=1:
+        return n
+    dp = [0] * (n+1)
+    dp[1] = 1
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    return dp
+
+
+print(fib(5))
